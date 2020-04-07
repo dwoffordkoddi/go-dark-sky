0dff3fc2ee0a369b00702aff12477ad4b0a51d3e
David Wofford <david.wofford@koddi.com>
2020-04-07 15:32:31 -0500

    Cleaning up the code a bit to make it easier to add new endpoints

--- endpoints/util.go
+++ endpoints/util.go
@@ -2,8 +2,12 @@ package endpoints
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	contracts "go-dark-sky/datacontracts"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func RespondWith(writer http.ResponseWriter, response contracts.CompletedJobResponse) {
@@ -18,4 +22,66 @@ func RespondWith(writer http.ResponseWriter, response contracts.CompletedJobResp
 		http.Error(writer, err.Error(), http.StatusBadRequest)
 		return
 	}
+}
+
+func getParameter(request *http.Request, field string, defaultValue string) string {
+	var parameter interface{}
+	var ok bool
+
+	if request.Method == http.MethodGet {
+		parameter, ok = request.URL.Query()[field]
+		parameter = fmt.Sprintf("%v", parameter)
+	} else {
+		parameter = request.Form.Get(field)
+		ok = true
+	}
+
+	if !ok || parameter == "" {
+		return defaultValue
+	}
+
+	return fmt.Sprintf("%v", parameter)
+}
+
+func validateRequired(request *http.Request, required []string) error {
+	var invalidParameters []string
+	errorMessage := "Missing required parameters: "
+
+	for i := 0; i < len(required); i++ {
+
+		if getParameter(request, required[i], "") == "" {
+			invalidParameters = append(invalidParameters, required[i])
+		}
+	}
+
+	if len(invalidParameters) > 0 {
+		return errors.New(errorMessage + strings.Join(invalidParameters, ", "))
+	}
+
+	return nil
+}
+
+func callApi(path string) (map[string]interface{}, error) {
+	var result map[string]interface{}
+
+	response, err := http.Get(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Body == nil {
+		result = nil
+		return nil, errors.New("unable to retrieve response")
+	} else {
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal(body, &result)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return result, nil
 }
--- endpoints/weather.go
+++ endpoints/weather.go
@@ -1,57 +1,38 @@
 package endpoints
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/KoddiDev/koddi-framework/endpoint"
 	"go-dark-sky/config"
 	contracts "go-dark-sky/datacontracts"
-	"io/ioutil"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
 func GetWeather() endpoint.Endpoint {
 	return endpoint.NewEndpoint("weather", http.MethodGet, "/weather", HandleGetWeatherRequest)
 }
 
-func HandleGetWeatherRequest(writer http.ResponseWriter, req *http.Request) {
+func HandleGetWeatherRequest(writer http.ResponseWriter, request *http.Request) {
+	var result map[string]interface{}
+	var apiError error
 	var errors []string
 
-	latitudeQuery, ok := req.URL.Query()["latitude"]
-	if !ok {
-		errors = append(errors, "Missing parameter latitude")
+	validationErrors := validateRequired(request, []string{"latitude", "longitude"})
+
+	if validationErrors != nil {
+		errors = append(errors, validationErrors.Error())
 	}
 
-	longitudeQuery, ok := req.URL.Query()["longitude"]
-	if !ok {
-		errors = append(errors, "Missing parameter longitude")
-	}
+	if errors == nil {
+		latitude, _ := strconv.ParseFloat(getParameter(request, "latitude", ""), 64)
+		longitude, _ := strconv.ParseFloat(getParameter(request, "longitude", ""), 64)
 
-	latitude, _ := strconv.ParseFloat(strings.Join(latitudeQuery, ""), 64)
-	longitude, _ := strconv.ParseFloat(strings.Join(longitudeQuery, ""), 64)
+		path := fmt.Sprintf("/forecast/%s/%f,%f", config.ServiceConf.DarkSky.ApiKey, latitude, longitude)
 
-	path := fmt.Sprintf("/forecast/%s/%f,%f", config.ServiceConf.DarkSky.ApiKey, latitude, longitude)
-
-	response, err := http.Get(config.ServiceConf.DarkSky.Url + path)
-	if err != nil {
-		errors = append(errors, err.Error())
-	}
-
-	var result map[string]interface{}
-
-	if response.Body == nil {
-		result = nil
-		errors = append(errors, "Unable to retrieve response")
-	} else {
-		body, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			errors = append(errors, err.Error())
-		}
-		err = json.Unmarshal(body, &result)
-		if err != nil {
-			errors = append(errors, err.Error())
+		result, apiError = callApi(config.ServiceConf.DarkSky.Url + path)
+		if apiError != nil {
+			errors = append(errors, apiError.Error())
 		}
 	}
 
